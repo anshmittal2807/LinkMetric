ae0b306ec5b83e7cea7026314ad34ad00f014e9d
ansh mittal <anshmittal280706@gmail.com>
2026-06-02 17:35:48 +0530

    fixed router component

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './main.css'
 import App from './App.jsx'
+import router from './router/index.jsx'
+import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+    </RouterProvider>
   </StrictMode>,
 )
--- FrontEnd/src/router/index.jsx
+++ FrontEnd/src/router/index.jsx
@@ -1,23 +1,29 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes , createBrowserRouter } from 'react-router-dom'
 import UserContextProvider from '../context/UserContextProvider'
 import HomePage from '../pages/HomePage'
 import SignupPage from '../pages/SignupPage'
 import LoginPage from '../pages/LoginPage'
 
-function AppRouter() {
-  return (
-    <UserContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/login" element={<LoginPage />} />
 
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </UserContextProvider>
-  )
-}
 
-export default AppRouter
+const router  = createBrowserRouter([
+  {
+    path : '/',
+    element : <HomePage />
+  },
+  {
+    path : '/signup', 
+    element : <SignupPage />
+  },
+  { 
+    path : '/login',
+    element : <LoginPage />
+  },
+  {
+    path : '*',
+    element : <Navigate to='/' replace />
+  }
+])
+
+
+export default router;
